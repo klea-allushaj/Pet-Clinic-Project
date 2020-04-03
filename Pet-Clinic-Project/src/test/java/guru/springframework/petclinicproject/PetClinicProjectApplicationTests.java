5c0fd806a021efef440ef9df9b34eae0e4b96a16
leartallushaj <leartallushaj14@gmail.com>
2020-04-03 20:49:14 +0200

    First Commit

---
+++ Pet-Clinic-Project/src/test/java/guru/springframework/petclinicproject/PetClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.petclinicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
